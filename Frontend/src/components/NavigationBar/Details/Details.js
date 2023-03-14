5b25a0b5c3b3df9ec0da78247fe18b9f38520762
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2023-03-15 01:32:11 +0200

    Font Family

--- Frontend/src/components/NavigationBar/Details/Details.js
+++ Frontend/src/components/NavigationBar/Details/Details.js
@@ -1,9 +1,7 @@
-import React from 'react' 
-import { Container ,Button ,NavLink } from 'react-bootstrap'
-import styles from './Details.module.css'
-import CircularImage from './CircularImage' 
-
-
+import React from "react";
+import { Container, Button, NavLink } from "react-bootstrap";
+import styles from "./Details.module.css";
+import CircularImage from "./CircularImage";
 
 const Details = () => {
   return (
@@ -13,22 +11,29 @@ const Details = () => {
         <h2 className={styles.text2}> about your health</h2>
         <CircularImage></CircularImage>
         <p className={styles.text3}>
-          Good health is the state of mental,
-          physical and social well being  </p>  
-        <p className={styles.text4}>and it does not just mean absence of diseases.</p>  
-        <Button className={styles.appointment}> Book an appointment
-          <i className='fas fa-arrow-right'></i> </Button>
-        <button className={styles.videoBtn} >
-          <i className={`fa-solid fa-play ${styles.playBtn}`}></i></button> watch video 
+          Good health is the state of mental, physical and social well being{" "}
+        </p>
+        <p className={styles.text4}>
+          and it does not just mean absence of diseases.
+        </p>
+        <Button className={styles.appointment}>
+          {" "}
+          Book an appointment
+          <i className="fas fa-arrow-right"></i>{" "}
+        </Button>
+        <button className={styles.videoBtn}>
+          <i className={`fa-solid fa-play ${styles.playBtn}`}></i>
+        </button>{" "}
+        watch video
         <p className={styles.member}>
           Become a member of out hosptial community?
-        <NavLink href='#link' className={styles.reg}>
-          Sign up
-        </NavLink> 
+          <NavLink href="#link" className={styles.reg}>
+            Sign up
+          </NavLink>
         </p>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
