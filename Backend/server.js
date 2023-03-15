e03399908a0facc37c62b6f4cd816311867571a9
abdelrahmanbauomyi <abdo01820@gmail.com>
2023-03-15 18:31:23 +0200

    add the cookie parser Back

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,10 @@ const { sequelize, User } = require('./models');
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/router');
+const cookieParser = require('cookie-parser');
 //TEST CODE FOR THE DATABASE CONNECTION
 app.use(express.json());
+app.use(cookieParser())
 
 //Routes
 app.use('/', userRoutes);
@@ -13,7 +15,7 @@ app.listen({ port: process.env.BACK_END_PORT }, async () => {
   console.log('running!!');
   await sequelize.sync(
     // commented the force so it doesnt remove the database everytime you run
-    { force: true }
+    // { force: true }
   );
   //await sequelize.sync();
   console.log('synced !!');
