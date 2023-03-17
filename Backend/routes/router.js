91d7fdbccd3add0fc8a4c9fb46824cba7d1d37c3
omar-tolan <omar.tolan@gmail.com>
2023-03-17 16:34:12 +0200

    added logout functionality

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -9,10 +9,9 @@ router.get('/users', userHandler.getUsers);
 router.get('/auth', authUser, userHandler.authTest);
 router.post('/users', userHandler.postUser);
 
-
 // Request for edit and delete user all should have the userId passed in the body
 // dummy urls for simple tests
-router.post('/users/delete',userHandler.postDeleteUser);
+router.post('/users/delete', userHandler.postDeleteUser);
 /* this should open the edit page and fill the 
     the values are 'firstName', 'lastName', 'email', 'gender', 'mobilenumber','dob'
     password wont be shown for security and the other fields are irrelevant user
@@ -20,10 +19,15 @@ router.post('/users/delete',userHandler.postDeleteUser);
 router.get('/users/edit', userHandler.getEditUser);
 /* after the user edit the data he will send a post request
  */
-router.post('/users/edit',userHandler.postEditUser);
-
+router.post('/users/edit', userHandler.postEditUser);
 
 //Request at /login
 router.get('/login', userHandler.loginUser);
+router.post('/logout', authUser, userHandler.logout);
+router.post(
+  '/logoutFromAllDevices',
+  authUser,
+  userHandler.logoutFromAllDevices
+);
 
 module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const userRoutes = require('./routes/router');
 const cookieParser = require('cookie-parser');
 //TEST CODE FOR THE DATABASE CONNECTION
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 //Routes
 app.use('/', userRoutes);
@@ -13,10 +13,11 @@ app.use('/', userRoutes);
 //Server
 app.listen({ port: process.env.BACK_END_PORT }, async () => {
   console.log('running!!');
-  await sequelize.sync(
+  await sequelize
+    .sync
     // commented the force so it doesnt remove the database everytime you run
     // { force: true }
-  );
+    ();
   //await sequelize.sync();
   console.log('synced !!');
 });
