936b18b4c1f36fc744d38ad24037727ae3b35413
abdelrahmanbauomyi <abdo01820@gmail.com>
2023-03-18 00:34:45 +0200

    prettifying the code

--- Backend/Handlers/userHandler.js
+++ Backend/Handlers/userHandler.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
 const { where } = require('sequelize');
 const { BCRYPT_PASSWORD, SALT_ROUNDS } = process.env;
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
 //GET Logic
 exports.getUsers = (req, res) => {
   User.findAll()
@@ -85,9 +85,9 @@ exports.postUser = async (req, res) => {
       // secure: true, set this on production
       sameSite: 'strict',
     });
-    const verUrl = `http://localhost:5000/confirmation/${token}`
+    const verUrl = `http://localhost:5000/confirmation/${token}`;
 
-    this.sendVerificationEmail(json.email,verUrl)
+    this.sendVerificationEmail(json.email, verUrl);
     return res.status(201).json(user);
   } catch (err) {
     return res.status(500).json(err);
@@ -219,38 +219,35 @@ exports.postDeleteUser = (req, res, next) => {
     .catch((error) => {
       console.log(error);
       res.send({ error: error });
-    })
-}
-exports.sendVerificationEmail= async (email,urlToken)=>{
+    });
+};
+exports.sendVerificationEmail = async (email, urlToken) => {
   const transporter = nodemailer.createTransport({
-    service:'gmail',
-    host:'smtp.gmail.com',
-    secure:false, 
-    auth:{
-      user: process.env.EMAIL_USER ,
-      pass: process.env.EMAIL_PASS
-    }
-  })
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
   await transporter.sendMail({
     from: 'Our Online Clinc',
     to: email,
     subject: 'Verify your email address',
-    html:`please click this url to confirm your email address : <a href=${urlToken}>${urlToken}</a>`
+    html: `please click this url to confirm your email address : <a href=${urlToken}>${urlToken}</a>`,
   });
-}
-exports.verifyEmail= async (req,res,next) => {
-  try{
-
+};
+exports.verifyEmail = async (req, res, next) => {
+  try {
     const token = req.params.token;
     console.log(token);
     const decoded = jwt.verify(token, process.env.JWT_STRING);
     const user = await User.findOne({ where: { id: decoded._id } });
-    console.log(user)
-    await User.update({confirmed: true},{where:{id:decoded._id}});
-    res.status(200).send("email has been verified")
-
+    console.log(user);
+    await User.update({ confirmed: true }, { where: { id: decoded._id } });
+    res.status(200).send('email has been verified');
+  } catch (err) {
+    res.send(err);
   }
-  catch(err){
-    res.send(err)
-  }
-}
+};
--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -11,22 +11,20 @@ router.post('/users', userHandler.postUser);
 
 // Request for edit and delete user all should have the userId passed in the body
 // dummy urls for simple tests
-router.delete('/users/delete',authUser,userHandler.postDeleteUser);
+router.delete('/users/delete', authUser, userHandler.postDeleteUser);
 /* this should open the edit page and fill the 
     the values are 'firstName', 'lastName', 'email', 'gender', 'mobilenumber','dob'
     password wont be shown for security and the other fields are irrelevant user
 */
-router.get('/users/edit',authUser, userHandler.getEditUser);
+router.get('/users/edit', authUser, userHandler.getEditUser);
 /* after the user edit the data he will send a post request
  */
-router.post('/users/edit',authUser,userHandler.postEditUser);
-
+router.post('/users/edit', authUser, userHandler.postEditUser);
 
 //Request at /login
 router.get('/login', userHandler.loginUser);
 
-
 //confirm email
-router.get('/confirmation/:token', userHandler.verifyEmail)
+router.get('/confirmation/:token', userHandler.verifyEmail);
 
 module.exports = router;
