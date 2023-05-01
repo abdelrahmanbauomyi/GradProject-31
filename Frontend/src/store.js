3f665f5bc7ff1a076dbde37156be8ad9bafe58c1
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2023-05-01 13:54:00 +0300

    Typo Fix

--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -1,22 +1,27 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import { userDetailsReducers, userLoginReducers, userRegisterReducers, userUpdateReducers } from "./reducers/auth";
+import {
+  userDetailsReducers,
+  userLoginReducers,
+  userRegisterReducers,
+  userUpdateReducers,
+} from "./reducers/auth";
 import { searchReducer } from "./reducers/searchReducer";
 
 const reducer = combineReducers({
   userLogin: userLoginReducers,
   searchFilters: searchReducer,
-  userRegister:userRegisterReducers,
-  userDetails:userDetailsReducers,
-  userUpdateProfile:userUpdateReducers
+  userRegister: userRegisterReducers,
+  userDetails: userDetailsReducers,
+  userUpdateProfile: userUpdateReducers,
 });
 const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
 const initialState = {
-  userlogin: { userInfo: userInfoFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
