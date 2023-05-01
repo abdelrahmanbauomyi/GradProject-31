954726cee1ef10eeefe527c7c780acb88edffa41
omar-tolan <omar.tolan@gmail.com>
2023-05-01 12:42:16 +0300

    fixed user editing bug

--- Backend/Handlers/doctorHandler.js
+++ Backend/Handlers/doctorHandler.js
@@ -44,18 +44,18 @@ exports.loginDoctor = async (req, res) => {
 };
 
 exports.Edit = async (req, res, next) => {
-  try{
+  try {
     debugger
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ["firstName", "lastName", "password", "email", "gender", "mobilenumber", "dob"]
+    const updates = Object.keys(req.body).filter((update) => req.body[update] !== "")
+    const allowedUpdates = ["Dname" , "password", "email", "gender", "mobilenumber", "dob"]
     const validUpdate = updates.every((update) => allowedUpdates.includes(update))
-    if(!validUpdate){
+    if (!validUpdate) {
       res.status(500).send("Invalid Updates!")
     }
     updates.forEach((update) => req.user[update] = req.body[update])
     req.user.save()
     res.status(200).send(req.user)
-  }catch(error){
+  } catch (error) {
     res.status(500).send(error)
   }
 }
@@ -84,7 +84,7 @@ exports.createDoctor = async (req, res) => {
       parseInt(SALT_ROUNDS)
     );
     const doctor = await Doctor.create(sentInfo);
-    const token = jwt.sign({_id: doctor.id, userType: "doctor"}, process.env.JWT_STRING);
+    const token = jwt.sign({ _id: doctor.id, userType: "doctor" }, process.env.JWT_STRING);
     await Doctor.update(
       {
         tokens: sequelize.fn('array_append', sequelize.col('tokens'), token),
@@ -103,151 +103,149 @@ exports.createDoctor = async (req, res) => {
 };
 
 
-exports.getAllDoctors = async (req,res) =>{
-  try{
+exports.getAllDoctors = async (req, res) => {
+  try {
     const doctors = await Doctor.findAll();
     res.status(200).json(doctors);
   }
-  catch(err){
+  catch (err) {
     return res.status(500).json(err)
   }
 
- 
+
 }
 
-exports.getDoctor = async (req,res) =>{
-  try{
-    const doctorID = req.body.id
-    const doctor = await Doctor.findByPk(doctorID)
-    res.status(200).json(doctor)
+exports.getDoctor = async (req, res) => {
+  try {
+    res.status(200).json(req.user)
   }
-  catch(err){
+  catch (err) {
     res.status(500).json(err)
 
   }
 }
 
-  exports.deleteDoctor = async (req,res) =>{
-    try{
-      const doctorID = req.body.id
-      const doctor = await Doctor.findByPk(doctorID)
-      if(doctor){
-        doctor.destroy()
-        res.status(200).json("Doctor removed successfully!")
-      }
-      else{
-        res.status(500).json("Doctor not found")
-
-      }
+exports.deleteDoctor = async (req, res) => {
+  try {
+    const doctorID = req.user.id
+    const doctor = await Doctor.findByPk(doctorID)
+    if (doctor) {
+      doctor.destroy()
+      res.status(200).json("Doctor removed successfully!")
     }
-    catch(err){
-      console.log(err)
-      res.status(500).json(err)
+    else {
+      res.status(500).json("Doctor not found")
+
     }
   }
+  catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
 
-  exports.logout = async (req, res) => {
-    try {
-      const validTokens = req.user.tokens.filter(
-        (token) => token !== req.cookies.token
-      );
-      await Doctor.update({ tokens: validTokens }, { where: { id: req.user.id } });
-      res.cookie('token', '', {
-        expires: new Date('October 13, 1970 11:13:00'),
-        httpOnly: true,
-        // secure: true, set this on production
-        sameSite: 'strict',
-      });
-      res.send(req.user);
-    } catch (e) {
-      res.send({ error: e });
-    }
-  };
+exports.logout = async (req, res) => {
+  try {
+    const validTokens = req.user.tokens.filter(
+      (token) => token !== req.cookies.token
+    );
+    await Doctor.update({ tokens: validTokens }, { where: { id: req.user.id } });
+    res.cookie('token', '', {
+      expires: new Date('October 13, 1970 11:13:00'),
+      httpOnly: true,
+      // secure: true, set this on production
+      sameSite: 'strict',
+    });
+    res.send(req.user);
+  } catch (e) {
+    res.send({ error: e });
+  }
+};
 
-  exports.logoutFromAllDevices = async (req, res) => {
-    try {
-      await Doctor.update({ tokens: [] }, { where: { id: req.user.id } });
-      res.cookie('token', '', {
-        expires: new Date('October 13, 1970 11:13:00'),
-        httpOnly: true,
-        // secure: true, set this on production
-        sameSite: 'strict',
-      });
-      res.send(req.user);
-    } catch (e) {
-      res.send({ error: e });
-    }
-  };
+exports.logoutFromAllDevices = async (req, res) => {
+  try {
+    await Doctor.update({ tokens: [] }, { where: { id: req.user.id } });
+    res.cookie('token', '', {
+      expires: new Date('October 13, 1970 11:13:00'),
+      httpOnly: true,
+      // secure: true, set this on production
+      sameSite: 'strict',
+    });
+    res.send(req.user);
+  } catch (e) {
+    res.send({ error: e });
+  }
+};
 
-  exports.searchDoctors = async (req,res) =>{
-    const {name , id , email ,dob , gender , mobilenumber , confirmed , rating , speciality , sub_specialties , title , area , location , fees , imgPath } = req.query.filters
-    const queryObj = {}
-    
-    if(name){
-      queryObj.Dname  = { [Op.like] : '%' + name + '%'}
-    }
-    if(id){
-      queryObj.id = id
-    }
-    if(email){
-      queryObj.email = email
-    }
-    if(dob){
-      queryObj.dob = dob
-    }
-    if(gender){
-      
-      queryObj.gender = gender
-    }
-    if(confirmed){
-      queryObj.confirmed = confirmed
-    }
-    if(rating){
-      queryObj.rating = rating
-    }
-    if(speciality){
-      queryObj.speciality = speciality
-    }
-    if(sub_specialties){
-      queryObj.sub_specialties = sub_specialties
-    }
-    if(title){
-      queryObj.title = title
-    }
-    if(area){
-      queryObj.area = area
-    }
-    if(location){
-      queryObj.location = location
-    }
-    if(fees){
-      queryObj.fees = fees
-    }
-    if(imgPath){
-      queryObj.imgPath = imgPath
-    }
-  
-    //console.log(queryObj)
-    try{
-      const doctors = await Doctor.findAll(
-        {
-          where: queryObj
-        }
-      )
-      res.status(200).json(doctors)
-      console.log(req.query)
-     
-    
+exports.searchDoctors = async (req, res) => {
+  const { name, id, email, dob, gender, mobilenumber, confirmed, rating, speciality, sub_specialties, title, area, location, fees, imgPath } = req.query.filters
+  const queryObj = {}
 
-    }
-    catch(err){
-      console.log(err)
-      res.status(500).json(err)
-    }
-  
-    
+  if (name) {
+    queryObj.Dname = { [Op.like]: '%' + name + '%' }
+  }
+  if (id) {
+    queryObj.id = id
+  }
+  if (email) {
+    queryObj.email = email
+  }
+  if (dob) {
+    queryObj.dob = dob
+  }
+  if (gender) {
+
+    queryObj.gender = gender
+  }
+  if (confirmed) {
+    queryObj.confirmed = confirmed
+  }
+  if (rating) {
+    queryObj.rating = rating
+  }
+  if (speciality) {
+    queryObj.speciality = speciality
+  }
+  if (sub_specialties) {
+    queryObj.sub_specialties = sub_specialties
+  }
+  if (title) {
+    queryObj.title = title
+  }
+  if (area) {
+    queryObj.area = area
+  }
+  if (location) {
+    queryObj.location = location
+  }
+  if (fees) {
+    queryObj.fees = fees
+  }
+  if (imgPath) {
+    queryObj.imgPath = imgPath
+  }
+
+  //console.log(queryObj)
+  try {
+    const doctors = await Doctor.findAll(
+      {
+        where: queryObj
+      }
+    )
+    res.status(200).json(doctors)
+    console.log(req.query)
 
 
 
   }
+  catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+
+
+
+
+
+}
 
