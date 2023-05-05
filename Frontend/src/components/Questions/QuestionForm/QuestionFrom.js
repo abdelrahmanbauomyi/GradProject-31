77af7a39e90660ae72612addc95cb9be1ef52038
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2023-05-06 00:51:29 +0300

    initial booking request

--- Frontend/src/components/Questions/QuestionForm/QuestionFrom.js
+++ Frontend/src/components/Questions/QuestionForm/QuestionFrom.js
@@ -13,6 +13,7 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 import Modal from "../../NavigationBar/Navbar/Modal";
 import headersConfig from "../../../utils/headersConfig";
+import Header from "../../Header/Header";
 
 export default function QuestionForm(props) {
   const [questionTitle, setQuestionTitle] = useState("");
@@ -23,90 +24,92 @@ export default function QuestionForm(props) {
   const { user } = userDetails;
   useEffect(() => {
     axios
-      .get("http://localhost:8000/faq", {
-        
-      })
+      .get("http://localhost:8000/faq", {})
       .then((result) => setFAQ((res) => result.data));
   }, []);
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    const config = headersConfig("qa")
-    axios.post("http://localhost:8000/qa", {
-      title: questionTitle,
-      question: questionContent,
-    } , config);
+    const config = headersConfig("qa");
+    axios.post(
+      "http://localhost:8000/qa",
+      {
+        title: questionTitle,
+        question: questionContent,
+      },
+      config
+    );
     setModal(true);
     setQuestionTitle("");
     setQuestionContent("");
   };
   return (
-    <MDBContainer
-      className="mt-5"
-      
-    >
-      <section>
-        {modal && (
-          <Modal
-            onClose={() => {
-              setModal(false);
-            }}
-          >
-            <p>You Question has been posted.</p>
-          </Modal>
-        )}
-        <MDBRow>
-          <MDBCol lg="6" md="12" className="mb-4">
-            <MDBContainer
-              className="mt-5"
-              style={
-                {
-                  /* maxWidth: "1000px" */
-                }
-              }
+    <>
+      <Header />
+      <MDBContainer className="mt-5">
+        <section>
+          {modal && (
+            <Modal
+              onClose={() => {
+                setModal(false);
+              }}
             >
-              <h3 style={{ textAlign: "center", marginBottom: "25px" }}>
-                Frequently Asked Questions
-              </h3>
-              <MDBAccordion alwaysOpen initialActive={1}>
-                {FAQ.map((question, idx) => (
-                  <MDBAccordionItem
-                    collapseId={idx + 1}
-                    headerTitle={question.question}
-                  >
-                    <p>{question.answer}</p>
-                  </MDBAccordionItem>
-                ))}
-              </MDBAccordion>
-            </MDBContainer>
-          </MDBCol>
-          <MDBCol lg="6" md="12" className="text-center mt-5">
-            <p>
-              <h5 class="fw-bold">
-                Still have any questions? Post your question and our staff will
-                answer.
-              </h5>
-            </p>
+              <p>You Question has been posted.</p>
+            </Modal>
+          )}
+          <MDBRow>
+            <MDBCol lg="6" md="12" className="mb-4">
+              <MDBContainer
+                className="mt-5"
+                style={
+                  {
+                    /* maxWidth: "1000px" */
+                  }
+                }
+              >
+                <h3 style={{ textAlign: "center", marginBottom: "25px" }}>
+                  Frequently Asked Questions
+                </h3>
+                <MDBAccordion alwaysOpen initialActive={1}>
+                  {FAQ.map((question, idx) => (
+                    <MDBAccordionItem
+                      collapseId={idx + 1}
+                      headerTitle={question.question}
+                    >
+                      <p>{question.answer}</p>
+                    </MDBAccordionItem>
+                  ))}
+                </MDBAccordion>
+              </MDBContainer>
+            </MDBCol>
+            <MDBCol lg="6" md="12" className="text-center mt-5">
+              <p>
+                <h5 class="fw-bold">
+                  Still have any questions? Post your question and our staff
+                  will answer.
+                </h5>
+              </p>
 
-            <form onSubmit={formSubmitHandler}>
-              <MDBInput
-                label="Question Title"
-                required
-                value = {questionTitle}
-                className="mb-4"
-                onChange={(event) => setQuestionTitle(event.target.value)}
-              />
-              <MDBTextArea
-                rows={4}
-                label="Your Question"
-                value = {questionContent}
-                className="mb-4"
-                onChange={(event) => setQuestionContent(event.target.value)}
-              />
-              <MDBBtn block>Post</MDBBtn>
-            </form>
-          </MDBCol>
-        </MDBRow>
-      </section>
-    </MDBContainer>
+              <form onSubmit={formSubmitHandler}>
+                <MDBInput
+                  label="Question Title"
+                  required
+                  value={questionTitle}
+                  className="mb-4"
+                  onChange={(event) => setQuestionTitle(event.target.value)}
+                />
+                <MDBTextArea
+                  rows={4}
+                  label="Your Question"
+                  value={questionContent}
+                  className="mb-4"
+                  onChange={(event) => setQuestionContent(event.target.value)}
+                />
+                <MDBBtn block>Post</MDBBtn>
+              </form>
+            </MDBCol>
+          </MDBRow>
+        </section>
+      </MDBContainer>
+    </>
   );
 }
--- Frontend/src/components/SearchPage/DoctorInfo/DoctorInfo.js
+++ Frontend/src/components/SearchPage/DoctorInfo/DoctorInfo.js
@@ -1,50 +1,80 @@
 import React from "react";
 import classes from "./DoctorInfo.module.css";
 import doctor from "../../../assets/doctor2.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStethoscope } from '@fortawesome/free-solid-svg-icons'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { faMoneyBill } from '@fortawesome/free-solid-svg-icons'
-import { faClock } from '@fortawesome/free-solid-svg-icons'
-import Rating from '@mui/material/Rating';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStethoscope } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faMoneyBill } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import Rating from "@mui/material/Rating";
 import Scheduler from "../../UI/Scheduler";
-
-
-
+import axios from "axios";
+import headersConfig from "../../../utils/headersConfig";
 
 function DoctorInfo() {
+  const bookingHandler = () => {
+    const config = headersConfig("PUT URL HERE")
+    axios.post("PUT URL HERE");
+  };
   return (
-<div>
-
-<div className={classes.gridcontainer}>
-  <div className={classes.item1}><h2>Doctor ma7ma 3askary</h2>
-        <p>Professor in Dermatology,askdhaskdhas,asdkahsdk</p>
-        <div className={classes.rating}>
-        <Rating
-        name="simple-controlled"
-        value={4.5}
-        readOnly
-       precision={0.5}
-      />
+    <div>
+      <div className={classes.gridcontainer}>
+        <div className={classes.item1}>
+          <h2>Doctor ma7ma 3askary</h2>
+          <p>Professor in Dermatology,askdhaskdhas,asdkahsdk</p>
+          <div className={classes.rating}>
+            <Rating
+              name="simple-controlled"
+              value={4.5}
+              readOnly
+              precision={0.5}
+            />
           </div>
         </div>
-  <div className={classes.item2}>
-  <div className={classes.img}>
-    <img src={doctor} alt="doctor" />
+        <div className={classes.item2}>
+          <div className={classes.img}>
+            <img src={doctor} alt="doctor" />
+          </div>
+        </div>
+        <div className={classes.item3}>
+          {" "}
+          <div className={classes.info2}>
+            <div>
+              <FontAwesomeIcon
+                className={classes.imgColor1}
+                icon={faStethoscope}
+              />
+              andrologist specialsidal asodiasodias{" "}
+            </div>
+            <div>
+              <FontAwesomeIcon
+                className={classes.imgColor1}
+                icon={faLocationDot}
+              />{" "}
+              el 3asher
+            </div>
+            <div>
+              <FontAwesomeIcon
+                className={classes.imgColor1}
+                icon={faMoneyBill}
+              />
+              500 LE
+            </div>
+            <div>
+              <FontAwesomeIcon className={classes.imgColor1} icon={faClock} /> 1
+              : 30 hours reamaining
+            </div>
+          </div>
+        </div>
+        <div className={classes.item4}>
+          {" "}
+          <Scheduler />
+          <Scheduler />
+          <Scheduler />
+        </div>
+      </div>
+      <button onClick={bookingHandler}></button>
     </div>
-    </div>
-  <div className={classes.item3}> <div className={classes.info2}>
-          <div><FontAwesomeIcon  className={classes.imgColor1}icon={faStethoscope} />andrologist specialsidal asodiasodias </div> 
-        <div><FontAwesomeIcon  className={classes.imgColor1} icon={faLocationDot} /> el 3asher</div>
-        <div><FontAwesomeIcon  className={classes.imgColor1} icon={faMoneyBill} />500 LE</div>
-        <div><FontAwesomeIcon className={classes.imgColor1} icon={faClock} /> 1 : 30 hours reamaining</div>
-        </div></div>  
-  <div className={classes.item4}> <Scheduler/>
-        <Scheduler/>
-        <Scheduler/></div>
-
-</div>
-</div>
   );
 }
 
