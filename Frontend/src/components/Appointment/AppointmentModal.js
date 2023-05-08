b82fdbf4bd20d28ee46b8cca27243961f0c0f0bb
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2023-05-08 07:44:34 +0300

    tweaks to appointment modal with some dummy data

--- Frontend/src/components/Appointment/AppointmentModal.js
+++ Frontend/src/components/Appointment/AppointmentModal.js
@@ -10,7 +10,51 @@ import headersConfig from "../../utils/headersConfig";
 import { useSelector } from "react-redux";
 import timeFormatter from "../../utils/timeFormatter";
 
-const AppointmentModal = ({ onClose, doctor }) => {
+const AppointmentModal = ({ onClose }) => {
+  const doctor = {
+    Dname: "Lotfy Mabrouk",
+    email: "maskedrider99xx@gmail.com",
+    dob: "1999-12-04T00:00:00.000Z",
+    gender: "Male",
+    mobilenumber: "11536520723",
+    confirmed: false,
+    rating: 0,
+    speciality: "Cardiothoracic",
+    sub_specialties: null,
+    title: "Dr",
+    area: null,
+    location: null,
+    fees: 200,
+    imgPath: "imgsdoctor.png",
+    createdAt: "2023-05-07T21:01:12.562Z",
+    updatedAt: "2023-05-07T21:02:07.449Z",
+    Bookings: [
+      {
+        id: 1,
+        startTime: "2016-01-22T19:00:00.000Z",
+        endTime: "2016-01-22T19:30:00.000Z",
+        status: "pending",
+      },
+      {
+        id: 2,
+        startTime: "2016-01-22T19:30:00.000Z",
+        endTime: "2016-01-22T20:00:00.000Z",
+        status: "pending",
+      },
+      {
+        id: 3,
+        startTime: "2016-01-22T20:00:00.000Z",
+        endTime: "2016-01-22T20:30:00.000Z",
+        status: "pending",
+      },
+      {
+        id: 4,
+        startTime: "2016-01-22T20:30:00.000Z",
+        endTime: "2016-01-22T21:00:00.000Z",
+        status: "pending",
+      },
+    ],
+  };
   const [bookings, setBookings] = useState([]);
   const [bookingId, setBookingId] = useState("");
   const { userInfo } = useSelector((state) => state.userLogin);
@@ -37,20 +81,12 @@ const AppointmentModal = ({ onClose, doctor }) => {
   return (
     <Modal onClose={onClose}>
       <Container>
-        <form onSubmit={bookingSubmitHandler}>
+        <form onSubmit={bookingSubmitHandler} style={{ textAlign: "center" }}>
           <h5>Booking Confirmation</h5>
-          <p>Doctor: {doctor.Dname}</p>
+          <h6>Doctor: {doctor.Dname}</h6>
           <div className="mb-4">
             <FormControl required variant="standard" sx={{ m: 1, width: 250 }}>
-              <InputLabel
-                id="demo-simple-select-standard-label"
-                sx={{
-                  display: "flex",
-                  flexDirection: "row-reverse",
-                  gap: "3px",
-                  marginLeft: "-15px",
-                }}
-              >
+              <InputLabel id="demo-simple-select-standard-label">
                 Day
               </InputLabel>
               <Select
@@ -59,12 +95,17 @@ const AppointmentModal = ({ onClose, doctor }) => {
                 value={bookingId}
                 onChange={handleChange}
                 label="Booking time"
+                sx={{
+                  color: "black",
+                }}
               >
-                {bookings.map((booking, idx) => (
-                  <MenuItem key={idx} value={booking.id}>
-                    {timeFormatter(booking.startTime)}
-                  </MenuItem>
-                ))}
+                {bookings
+                  .filter((booking) => booking.status === "pending")
+                  .map((booking, idx) => (
+                    <MenuItem key={idx} value={booking.id}>
+                      {timeFormatter(booking.startTime)}
+                    </MenuItem>
+                  ))}
               </Select>
             </FormControl>
           </div>
--- Frontend/src/utils/timeFormatter.js
+++ Frontend/src/utils/timeFormatter.js
@@ -1,12 +1,13 @@
 export default function timeFormatter(time) {
   const date = new Date(time);
-  const options = { timeZone: "Africa/Cairo", weekday: "short" };
-  const dayString = date.toLocaleDateString("en-US", options);
-  const dateString = date.toLocaleDateString("en-US", {
+  const options = {
+    weekday: "long",
+    month: "short",
+    day: "numeric",
     timeZone: "Africa/Cairo",
-  });
-  const timeString = date.toLocaleTimeString("en-US", {
-    timeZone: "Africa/Cairo",
-  });
-  return `${dayString}, ${dateString}, ${timeString}`;
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  const dateString = date.toLocaleDateString("en-US", options);
+  return `${dateString}`;
 }
