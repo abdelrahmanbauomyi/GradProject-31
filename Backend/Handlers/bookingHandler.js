c24e972be3b136b7da68cf6d895b071b16f2b40e
abdelrahmanbauomyi <abdo01820@gmail.com>
2023-06-25 02:46:58 +0300

    bug fix in the reservation

--- Backend/Handlers/bookingHandler.js
+++ Backend/Handlers/bookingHandler.js
@@ -1,5 +1,5 @@
 const dayjs = require('dayjs');
-const stripe = require('stripe')(process.env.STRIPE_KEY)
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 const { sequelize, Booking, User, Doctor } = require('../models');
 const { where } = require('sequelize');
 //doctor methods
@@ -9,7 +9,7 @@ exports.addAppointment = async (req, res) => {
       let startTime = dayjs(req.body.startTime);
       let endTime = dayjs(req.body.endTime);
       const oldTiming = dayjs(); //GMT+2
-      const now = oldTiming.add(1,'h')//GMT+3
+      const now = oldTiming.add(1, 'h'); //GMT+3
       if (startTime < now || endTime < now || startTime > endTime) {
         console.log(startTime, endTime, now);
         return res.status(400).json('Bad request');
@@ -17,19 +17,19 @@ exports.addAppointment = async (req, res) => {
       let diff = Math.abs(startTime - endTime);
       let Duration = req.body.duration * 60000; // duration in milliseconds
       let numberOfTimeSlots = Math.floor(diff / Duration);
-      let arr =[]
+      let arr = [];
       const doctorId = req.user.id;
       while (numberOfTimeSlots--) {
         endTime = startTime.add(req.body.duration, 'm');
-        
+
         arr.push({
           startTime: startTime,
           endTime: endTime,
           DoctorId: doctorId,
-        })
+        });
         startTime = endTime;
-      }      
-      const booking = await Booking.bulkCreate(arr)
+      }
+      const booking = await Booking.bulkCreate(arr);
       res.status(201).json(booking);
     } else {
       return res.status(401).json('unauthorized request');
@@ -79,13 +79,13 @@ exports.doctorHistory = async (req, res) => {
         ],
       });
 
-      const statusOrder = ["ongoing", "reserved", "pending", "Finished"];
+      const statusOrder = ['ongoing', 'reserved', 'pending', 'Finished'];
       const statusComparator = (a, b) => {
         const aIndex = statusOrder.indexOf(a.status);
         const bIndex = statusOrder.indexOf(b.status);
         return aIndex - bIndex;
       };
-      resualt.sort(statusComparator)
+      resualt.sort(statusComparator);
 
       res.status(200).json(resualt);
     } else {
@@ -97,7 +97,7 @@ exports.doctorHistory = async (req, res) => {
   }
 };
 // users methods
-exports.payAppointment = async (req,res)=>{
+exports.payAppointment = async (req, res) => {
   try {
     if (req.user.userType == 'user') {
       const userId = req.user.id;
@@ -106,29 +106,35 @@ exports.payAppointment = async (req,res)=>{
         { UserId: userId, status: 'pending Payment' },
         { where: { appointmentId: a_Id } }
       );
-    const appointment = await Booking.findByPk(a_Id);
-    const doctorId = appointment.DoctorId;
-    const doctor = await  Doctor.findByPk(doctorId);
-    const line_items = [{
-      price_data: {
-        currency: 'egp',
-        product_data: {
-          name: 'appointment with Dr ' + doctor.Dname,
-          description: doctor.speciality
+      const appointment = await Booking.findByPk(a_Id);
+      const doctorId = appointment.DoctorId;
+      const doctor = await Doctor.findByPk(doctorId);
+      const line_items = [
+        {
+          price_data: {
+            currency: 'egp',
+            product_data: {
+              name: 'appointment with Dr ' + doctor.Dname,
+              description: doctor.speciality,
+            },
+            unit_amount: doctor.fees * 100,
+          },
+          quantity: 1,
         },
-        unit_amount: doctor.fees * 100,
-      },
-      quantity: 1,
-    }];
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types:["card"],
-      mode: 'payment',
-      line_items,
-      success_url: `${req.protocol}://${req.get('host')}/checkout-success?verified=${process.env.SECRET_PAYMENT_CHECK}&appointmentId=${a_Id}&userId=${userId}` ,
-      cancel_url: `${req.protocol}://${req.get('host')}/checkout-cancel`,
-    });
- 
-    res.status(303).json({url : session.url});
+      ];
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'payment',
+        line_items,
+        success_url: `${req.protocol}://${req.get(
+          'host'
+        )}/checkout-success?verified=${
+          process.env.SECRET_PAYMENT_CHECK
+        }&appointmentId=${a_Id}&userId=${userId}`,
+        cancel_url: `${req.protocol}://${req.get('host')}/checkout-cancel`,
+      });
+
+      res.status(303).json({ url: session.url });
     } else {
       return res.status(401).json('unauthorized request');
     }
@@ -136,8 +142,7 @@ exports.payAppointment = async (req,res)=>{
     console.log(err);
     return res.status(500).json(err);
   }
-
-}
+};
 
 //still needs to be tested
 /*
@@ -168,8 +173,11 @@ exports.reserveAppointment = async (req, res) => {
         { UserId: userId, status: 'reserved' },
         { where: { appointmentId: a_Id } }
       );
+      if (result == false) {
+        res.status(404).json('appointment not found');
+      }
       res.status(201).json('reserved');
-  } else {
+    } else {
       return res.status(401).json('unauthorized request');
     }
   } catch (err) {
@@ -177,9 +185,6 @@ exports.reserveAppointment = async (req, res) => {
     return res.status(500).json(err);
   }
 };
-
-
-
 
 exports.showAvailable = async (req, res) => {
   try {
@@ -213,13 +218,13 @@ exports.userHistory = async (req, res) => {
           },
         ],
       });
-      const statusOrder = ["ongoing", "reserved", "pending", "Finished"];
+      const statusOrder = ['ongoing', 'reserved', 'pending', 'Finished'];
       const statusComparator = (a, b) => {
         const aIndex = statusOrder.indexOf(a.status);
         const bIndex = statusOrder.indexOf(b.status);
         return aIndex - bIndex;
       };
-      resualt.sort(statusComparator)
+      resualt.sort(statusComparator);
       res.status(200).json(resualt);
     } else {
       return res.status(401).json('unauthorized request');
